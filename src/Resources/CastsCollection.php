62da8288888d164efa1d61abdeb31b83d14d0c3f
Ashton Smith <ashton.smith@ubteam.com>
2025-01-23 08:03:40 +1300

    collectionCaster

---
+++ src/Resources/CastsCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace UbtAshton\Lightspeed\Resources;
+
+trait CastsCollection {
+    public function __construct(array $data= [])
+    {
+        $resource = substr(get_class($this),0,-10);
+        $collection = [];
+        foreach($data as $item){
+            $collection[] = new $resource($item);
+        }
+        parent::__construct($collection);
+    }
+}
